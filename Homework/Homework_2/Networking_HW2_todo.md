d50e60c5a439296f739cab5eaa3fce4674e1fc3a
Bholanath Das Niloy <drakensang47@gmail.com>
2023-02-16 00:10:18 +0600

    Changed DNS Server (main)

--- Homework/Homework_2/Networking_HW2_todo.md
+++ Homework/Homework_2/Networking_HW2_todo.md
@@ -1,7 +1,7 @@
 From the course book **Networking A Top-Down Approach** $8^{th}$ edition do problems $3, 6, 12, 14, 16, 19, 26, 27, 40, 44, 45, 47, 48, 52, 53$ from pages $287-298$.
 
 `Progress`:
-$3$
+$3, 6, 12,$
 
 `Remainging`:
-$6, 12, 14, 16, 19, 26, 27, 40, 44, 45, 47, 48, 52, 53$ from pages $287-298$
+$14, 16, 19, 26, 27, 40, 44, 45, 47, 48, 52, 53$
--- Labs/Lab_04/new_server.py
+++ Labs/Lab_04/new_server.py
@@ -1,27 +1,64 @@
-import dnslib
 import socket
+import dns.message
 
-mp = {
-    "cs.du.ac.bd." : "192.0.2.1"
+mpA = {
+    "cse.du.ac.bd." : "192.0.2.3",
+    "ns1.cse.du.ac.bd." : "192.0.2.1",
+    "ns2.cse.du.ac.bd." : "192.0.2.2",
+    "mail.cse.du.ac.bd." : "192.0.2.4"
 }
 
-def handle_dns_request(data, address):
-    request = dnslib.DNSRecord.parse(data)
-    domain_name = request.q.qname
-    response = dnslib.DNSRecord(dnslib.DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
-    response.add_answer(dnslib.RR(request.q.qname, ttl=60, rdata=dnslib.A(mp[str(domain_name)])))
-    return response.pack()
+mpNS = {
+    "cse.du.ac.bd." : "ns1.cse.du.ac.bd.",
+}
 
-def run_dns_server():
+mpAAAA = {
+    "cse.du.ac.bd." : "2001:db8::3",
+    "ns1.cse.du.ac.bd." : "2001:db8::1",
+    "ns2.cse.du.ac.bd." : "2001:db8::2",
+    "mail.cse.du.ac.bd." : "2001:db8::4"
+}
+
+mpCNAME = {
+    "www.cse.du.ac.bd." : "cse.du.ac.bd."    
+}
+
+mpMX = {
+    "cse.du.ac.bd." : "10 mail.cse.du.ac.bd."
+}
+
+def handle_request(data, client_address):
+    request = dns.message.from_wire(data)
+
+    response = dns.message.make_response(request)
+    for item in request.question:
+        domain_name, qtype = item.to_text().split(" IN ")
+        if qtype == "NS":
+            rrset = dns.rrset.from_text(domain_name, 86400, dns.rdataclass.IN, qtype, mpNS[domain_name]);
+            response.authority.append(rrset)
+        elif qtype == "AAAA":
+            rrset = dns.rrset.from_text(domain_name, 86400, dns.rdataclass.IN, qtype, mpAAAA[domain_name]);
+            response.answer.append(rrset)
+        elif qtype == "CNAME":
+            rrset = dns.rrset.from_text(domain_name, 86400, dns.rdataclass.IN, qtype, mpCNAME[domain_name]);
+            response.answer.append(rrset)
+        elif qtype == "MX":
+            rrset = dns.rrset.from_text(domain_name, 86400, dns.rdataclass.IN, qtype, mpMX[domain_name]);
+            response.answer.append(rrset)
+        else:
+            rrset = dns.rrset.from_text(domain_name, 86400, dns.rdataclass.IN, qtype, mpA[domain_name]);
+            response.answer.append(rrset)
+        print(f"DEBUG: Query for {domain_name} with type {qtype}")
+
+    return response.to_wire()
+
+def run_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('', 5050))
-
-    print("DNS server is listening on port 53...")
-
+    server_socket.bind(("127.0.0.1", 5050))
+    print("Server starting on 127.0.0.1 on port 5050")
     while True:
-        data, address = server_socket.recvfrom(4096)
-        response = handle_dns_request(data, address)
-        server_socket.sendto(response, address)
+        data, client_address = server_socket.recvfrom(4096)
+        response = handle_request(data, client_address)
+        server_socket.sendto(response, client_address)
 
-if __name__ == "__main__":
-    run_dns_server()
+run_server()
