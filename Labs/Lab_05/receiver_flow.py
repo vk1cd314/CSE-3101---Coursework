af20a9905860e43268f70c5f4163c4a6fd8d99c2
Bholanath Das Niloy <drakensang47@gmail.com>
2023-02-19 13:19:21 +0600

    Flow control fin

---
+++ Labs/Lab_05/receiver_flow.py
@@ -0,0 +1,65 @@
+import socket
+
+def toHeader(seqNum=0, ackNum=0, ack=0, sf=0, rwnd=0):
+    return seqNum.to_bytes(
+        4, byteorder="little") + ackNum.to_bytes(
+            4, byteorder="little") + ack.to_bytes(
+                1, byteorder="little") + sf.to_bytes(
+                    1, byteorder="little") + rwnd.to_bytes(
+                        2, byteorder="little")
+
+def fromHeader(segment):
+    return int.from_bytes(
+        segment[:4], byteorder="little"), int.from_bytes(
+            segment[4:8], byteorder="little"), int.from_bytes(
+                segment[8:9], byteorder="little"), int.from_bytes(
+                    segment[9:10], byteorder="little"), int.from_bytes(
+                        segment[10:12], byteorder="little")
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('', 8888)
+server_socket.bind(server_address)
+
+server_socket.listen(1)
+
+client_socket, client_address = server_socket.accept()
+
+recv_buffer_size = 12
+window_size = 4 * recv_buffer_size
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, recv_buffer_size)
+
+expected_seq_num = 0
+ack_num = 0
+
+received_data = b''
+while True:
+    header = client_socket.recv(12)
+    seq_num, ack_numm, ack, sf, rwnd = fromHeader(header)
+    print(seq_num, ack_num, ack, sf, rwnd)
+    
+    data = client_socket.recv(rwnd)
+    print(data.decode())
+    
+    if not data:
+        break
+    
+    seq_num = ack_num
+    
+    if seq_num == expected_seq_num:
+        received_data += data
+        
+        ack_num += len(data)
+        expected_seq_num += len(data)
+        
+        to_send_ack = toHeader(seq_num, ack_num, 1, 0, 12)
+        client_socket.sendall(to_send_ack)
+    else:
+        to_send_ack = toHeader(seq_num, ack_num, 1, 0, 12)
+        client_socket.sendall(to_send_ack)
+
+print(received_data.decode())
+
+client_socket.close()
+server_socket.close()
---
+++ Labs/Lab_05/sender_flow.py
@@ -0,0 +1,55 @@
+import socket
+import time
+
+def toHeader(seqNum=0, ackNum=0, ack=0, sf=0, rwnd=0):
+    return seqNum.to_bytes(
+        4, byteorder="little") + ackNum.to_bytes(
+            4, byteorder="little") + ack.to_bytes(
+                1, byteorder="little") + sf.to_bytes(
+                    1, byteorder="little") + rwnd.to_bytes(
+                        2, byteorder="little")
+
+def fromHeader(segment):
+    return int.from_bytes(
+        segment[:4], byteorder="little"), int.from_bytes(
+            segment[4:8], byteorder="little"), int.from_bytes(
+                segment[8:9], byteorder="little"), int.from_bytes(
+                    segment[9:10], byteorder="little"), int.from_bytes(
+                        segment[10:12], byteorder="little")
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('127.0.0.1', 8888)
+client_socket.connect(server_address)
+
+recv_buffer_size = 2
+window_size = 4 * recv_buffer_size
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, recv_buffer_size)
+
+seq_num = 0
+expected_ack_num = 0
+
+data = b'This is a sample message to test the flow control algorithm, it just needs to be long enough to test the flow control algorithm.'
+data_len = len(data)
+
+timeout = 2  # in seconds
+start_time = time.time()
+
+while expected_ack_num < data_len:
+    send_size = min(window_size, data_len - expected_ack_num)
+
+    client_socket.sendall(toHeader(seq_num, expected_ack_num, 1, 0, send_size) + data[seq_num:seq_num + send_size])
+    
+    ack_header = client_socket.recv(12)
+    
+    seqNum, ack_num, ack, sf, rwnd = fromHeader(ack_header)
+
+    seq_num += send_size
+    expected_ack_num = ack_num
+    
+    if time.time() - start_time > timeout:
+        seq_num = expected_ack_num
+        start_time = time.time()
+
+client_socket.close()
